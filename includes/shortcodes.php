3e0508277b31bdc5b8955e3e4d57f2e1846b773d
wikitopian <matt@swarmstrategies.com>
2012-08-12 23:47:33 -0600

    Fix and simplify shortcode

--- includes/shortcodes.php
+++ includes/shortcodes.php
@@ -3,27 +3,15 @@
 /* Shortcode Generator                  */
 /* ------------------------------------ */
 
-
 // Shortcode [perfect_quotes id="10"]
 function perfect_quotes_func($atts) {
-  extract(shortcode_atts(array('id' => null, 'num' => null, 'random' => null), $atts));
-  if ($id == null && $num == null & $random == null) {
-    return false;
-  } else if ($id){
+  extract(shortcode_atts(array('id' => null), $atts));
+  if ($id){
     $args = array(
       'p'         => $id,
       'post_type' => 'perfect-quotes'
     );
-  } else if ($num) {
-    $num = ($num == 'all') ? -1 : $num;
-    $args = array(
-      'posts_per_page' => $num,
-      'post_type'      => 'perfect-quotes'
-    );
-    if ($random) {
-      $args['orderby'] = 'rand';
-    }
-  } else if ($random) {
+  } else {
     $args = array(
       'posts_per_page' => 1,
       'orderby'   => 'rand',
@@ -41,16 +29,8 @@ function perfect_quotes_func($atts) {
   while ($query->have_posts()) {
     $query->the_post();
     echo '<div class="perfect-quotes">';
-    if (get_the_title()) the_title();
-    $quote_author = get_post_meta(get_the_ID(), 'perfect_quote_author', true);
-    $quote_where  = get_post_meta(get_the_ID(), 'perfect_quote_where', true);
-    if (!empty($quote_author) || !empty($quote_where)) {
-      echo '<span>- ';
-      if (!empty($quote_author)) echo $quote_author;
-      if (!empty($quote_author) && !empty($quote_where)) echo ', ';
-      if (!empty($quote_where)) echo $quote_where;
-      echo '</span>';
-    }
+	echo get_the_content();
+    echo '<span>- '.get_the_title().'</span>';
     echo '</div>';
   }
 
