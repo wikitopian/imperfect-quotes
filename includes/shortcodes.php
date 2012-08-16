5931510016ef99b4ae6e0039500e96dc0d12ba31
wikitopian <matt@swarmstrategies.com>
2012-08-15 18:39:53 -0600

    Self-documenting function

--- includes/shortcodes.php
+++ includes/shortcodes.php
@@ -1,10 +1,7 @@
 <?php
-/* ------------------------------------ */
-/* Shortcode Generator                  */
-/* ------------------------------------ */
 
-// Shortcode [imperfect_quotes id="10"]
-function imperfect_quotes_func($atts) {
+// Shortcode [imperfect_quotes id="10" image_width="100" image_height="100"]
+function imperfect_quotes_shortcodes($atts) {
 	global $imperfect_quote_image_width;
 	global $imperfect_quote_image_height;
 	$image_width = $imperfect_quote_image_width;
@@ -20,7 +17,6 @@ function imperfect_quotes_func($atts) {
 		)
 	);
 
-  // Load Imperfect Quotes style.css
   wp_enqueue_style('imperfect_quotes', plugins_url('style.css', __FILE__));
 
   ob_start();
@@ -32,4 +28,4 @@ function imperfect_quotes_func($atts) {
   return $content;
 }
 
-add_shortcode('imperfect_quotes', 'imperfect_quotes_func');
+add_shortcode('imperfect_quotes', 'imperfect_quotes_shortcodes');
