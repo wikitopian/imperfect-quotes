f6e85db6c36797360cccb077833235f557deb7fb
wikitopian <matt@swarmstrategies.com>
2012-08-15 18:50:25 -0600

    Reformat

--- includes/widget.php
+++ includes/widget.php
@@ -2,106 +2,106 @@
 add_action('widgets_init', 'imperfect_quotes_register_widgets');
 
 function imperfect_quotes_register_widgets() {
-  register_widget('Imperfect_Quotes_Widget');
+	register_widget('Imperfect_Quotes_Widget');
 }
 
 class Imperfect_Quotes_Widget extends WP_Widget {
 
-  function __construct() {
-    $widget_ops = array(
-      'classname' => 'widget_imperfect_quote',
-      'description' => __('A quotes widget imperfectly done'),
-    );
-    parent::__construct('imperfect-quotes', __('Imperfect Quotes'), $widget_ops);
-    $this->alt_option_name = 'widget_imperfect_quote';
+	function __construct() {
+		$widget_ops = array(
+			'classname' => 'widget_imperfect_quote',
+			'description' => __('A quotes widget imperfectly done'),
+		);
+		parent::__construct('imperfect-quotes', __('Imperfect Quotes'), $widget_ops);
+		$this->alt_option_name = 'widget_imperfect_quote';
 
-    add_action( 'save_post', array(&$this, 'flush_widget_cache') );
-    add_action( 'deleted_post', array(&$this, 'flush_widget_cache') );
-    add_action( 'switch_theme', array(&$this, 'flush_widget_cache') );
-  }
-
-  function widget($args, $instance) {
-    // Retrieve cached data
-    $cache = wp_cache_get('widget_imperfect_quotes', 'widget');
-
-    // Load Imperfect Quotes style.css
-    wp_enqueue_style('imperfect_quotes', plugins_url('style.css', __FILE__));
-
-    if (!is_array($cache)) {
-      $cache = array();
-    }
-
-    if (isset($cache[$args['widget_id']])) {
-      echo $cache[$args['widget_id']];
-      return;
-    }
-
-    // We don't have cached data : we create it!
-    ob_start();
-    extract($args);
-    $title = apply_filters('widget_title', empty($instance['title']) ? __('Imperfect Quotes') : $instance['title'], $instance, $this->id_base);
-
-    if (!$number = absint($instance['number'])) {
-      $number = 1;
-    }
-
-	echo $before_widget;
-	if ($title) {
-		echo $before_title . $title . $after_title;
+		add_action( 'save_post', array(&$this, 'flush_widget_cache') );
+		add_action( 'deleted_post', array(&$this, 'flush_widget_cache') );
+		add_action( 'switch_theme', array(&$this, 'flush_widget_cache') );
 	}
 
-	global $imperfect_quote_image_width,
-	global $imperfect_quote_image_height
-	echo imperfect_quotes_get_quote(
-		null,
-		$imperfect_quote_image_width,
-		$imperfect_quote_image_height
-	);
+	function widget($args, $instance) {
+		// Retrieve cached data
+		$cache = wp_cache_get('widget_imperfect_quotes', 'widget');
 
-	echo $after_widget;
+		// Load Imperfect Quotes style.css
+		wp_enqueue_style('imperfect_quotes', plugins_url('style.css', __FILE__));
 
-    // Echo the result get it for caching
-    $cache[$args['widget_id']] = ob_get_flush();
-    wp_cache_set('widget_imperfect_quotes', $cache, 'widget');
+		if (!is_array($cache)) {
+			$cache = array();
+		}
+
+		if (isset($cache[$args['widget_id']])) {
+			echo $cache[$args['widget_id']];
+			return;
+		}
+
+		// We don't have cached data : we create it!
+		ob_start();
+		extract($args);
+		$title = apply_filters('widget_title', empty($instance['title']) ? __('Imperfect Quotes') : $instance['title'], $instance, $this->id_base);
+
+		if (!$number = absint($instance['number'])) {
+			$number = 1;
+		}
+
+		echo $before_widget;
+		if ($title) {
+			echo $before_title . $title . $after_title;
+		}
+
+		global $imperfect_quote_image_width,
+			global $imperfect_quote_image_height
+			echo imperfect_quotes_get_quote(
+				null,
+				$imperfect_quote_image_width,
+				$imperfect_quote_image_height
+			);
+
+		echo $after_widget;
+
+		// Echo the result get it for caching
+		$cache[$args['widget_id']] = ob_get_flush();
+		wp_cache_set('widget_imperfect_quotes', $cache, 'widget');
 	}
 
-  function update($new_instance, $old_instance) {
-    $instance = $old_instance;
-    $instance['title'] = strip_tags($new_instance['title']);
-    $instance['number'] = (int) $new_instance['number'];
-    $instance['random'] = strip_tags($new_instance['random']);
-    // Keep the data fresh
-    $this->flush_widget_cache();
+	function update($new_instance, $old_instance) {
+		$instance = $old_instance;
+		$instance['title'] = strip_tags($new_instance['title']);
+		$instance['number'] = (int) $new_instance['number'];
+		$instance['random'] = strip_tags($new_instance['random']);
+		// Keep the data fresh
+		$this->flush_widget_cache();
 
-    $alloptions = wp_cache_get('alloptions', 'options');
-    if (isset($alloptions['widget_imperfect_quote'])) {
-      delete_option('widget_imperfect_quote');
-    }
+		$alloptions = wp_cache_get('alloptions', 'options');
+		if (isset($alloptions['widget_imperfect_quote'])) {
+			delete_option('widget_imperfect_quote');
+		}
 
-    return $instance;
-  }
+		return $instance;
+	}
 
-  function flush_widget_cache() {
-    wp_cache_delete('widget_imperfect_quotes', 'widget');
-  }
+	function flush_widget_cache() {
+		wp_cache_delete('widget_imperfect_quotes', 'widget');
+	}
 
-  function form($instance) {
-    $title = isset($instance['title']) ? esc_attr($instance['title']) : '';
-    $number = isset($instance['number']) ? absint($instance['number']) : 1;
-    $random = esc_attr( $instance['random']);
-    ?>
-    <p>
-      <label for="<?php echo $this->get_field_id('title'); ?>"><?php echo __('Title:'); ?></label>
-      <input class="widefat" id="<?php echo $this->get_field_id('title'); ?>" name="<?php echo $this->get_field_name('title'); ?>" type="text" value="<?php echo $title; ?>" />
-    </p>
-    <p>
-      <label for="<?php echo $this->get_field_id('number'); ?>"><?php echo __('Number of quotes to show:'); ?></label>
-      <input id="<?php echo $this->get_field_id('number'); ?>" name="<?php echo $this->get_field_name('number'); ?>" type="text" value="<?php echo $number; ?>" size="3" />
-    </p>
-    <p>
-      <input type="checkbox" class="checkbox" name="<?php echo $this->get_field_name('random')?>" value="1" <?php checked( $random, 1 ); ?> />
-      <label for="<?php echo $this->get_field_id('random'); ?>"><?php _e('Display random quote'); ?></label>
-    </p>
-    <?php
-  }
+	function form($instance) {
+		$title = isset($instance['title']) ? esc_attr($instance['title']) : '';
+		$number = isset($instance['number']) ? absint($instance['number']) : 1;
+		$random = esc_attr( $instance['random']);
+?>
+	<p>
+	  <label for="<?php echo $this->get_field_id('title'); ?>"><?php echo __('Title:'); ?></label>
+	  <input class="widefat" id="<?php echo $this->get_field_id('title'); ?>" name="<?php echo $this->get_field_name('title'); ?>" type="text" value="<?php echo $title; ?>" />
+	</p>
+	<p>
+	  <label for="<?php echo $this->get_field_id('number'); ?>"><?php echo __('Number of quotes to show:'); ?></label>
+	  <input id="<?php echo $this->get_field_id('number'); ?>" name="<?php echo $this->get_field_name('number'); ?>" type="text" value="<?php echo $number; ?>" size="3" />
+	</p>
+	<p>
+	  <input type="checkbox" class="checkbox" name="<?php echo $this->get_field_name('random')?>" value="1" <?php checked( $random, 1 ); ?> />
+	  <label for="<?php echo $this->get_field_id('random'); ?>"><?php _e('Display random quote'); ?></label>
+	</p>
+<?php
+	}
 }
